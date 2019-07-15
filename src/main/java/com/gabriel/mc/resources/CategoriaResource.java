46304ca87e461f0f937907eecb80982c6ccdc009
Gabriel Longhi <gabrielkuodrek@hotmail.com>
2019-07-15 10:57:52 -0300

    Primeiro commit

---
+++ src/main/java/com/gabriel/mc/resources/CategoriaResource.java
@@ -0,0 +1,29 @@
+package com.gabriel.mc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gabriel.mc.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> listar() {
+		
+		Categoria cat = new Categoria(1,"Informática..") ;
+		Categoria cat2 = new Categoria(2,"Escritorio..") ;
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat);
+		lista.add(cat2);
+		
+		return lista;
+	} 
+}
