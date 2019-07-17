4a4db7211ecb5b088511ee4aad14885ab81ac385
Gabriel Longhi <gabrielkuodrek@hotmail.com>
2019-07-17 14:11:12 -0300

    Nova Categoria com method POST

--- src/main/java/com/gabriel/mc/resources/CategoriaResource.java
+++ src/main/java/com/gabriel/mc/resources/CategoriaResource.java
@@ -3,12 +3,12 @@ package com.gabriel.mc.resources;
 import com.gabriel.mc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.gabriel.mc.domain.Categoria;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -21,5 +21,17 @@ public class  CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
-	} 
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(obj.getId())
+				.toUri();
+
+		return ResponseEntity.created(uri).build();
+	}
 }
