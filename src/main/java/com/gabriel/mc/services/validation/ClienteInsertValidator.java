4965d816e87b738f8e53fa9d3cac94b2abe03440
Gabriel Longhi <gabrielkuodrek@hotmail.com>
2019-07-22 18:29:23 -0300

    Em desenvolvimento

---
+++ src/main/java/com/gabriel/mc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,33 @@
+package com.gabriel.mc.services.validation;
+
+import com.gabriel.mc.dto.ClienteNewDTO;
+import com.gabriel.mc.resources.exceptions.FieldMessage;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+    @Override
+    public void initialize(ClienteInsert ann) {
+    }
+
+    @Override
+    public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+        List<FieldMessage> list = new ArrayList<>();
+
+        if (objDto.getTipo() == null) {
+            list.add(new FieldMessage("tipo", "Tipo não pode ser nulo"));
+        }
+
+        // inclua os testes aqui, inserindo erros na lista
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage())
+                    .addPropertyNode(e.getFieldName()).addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
