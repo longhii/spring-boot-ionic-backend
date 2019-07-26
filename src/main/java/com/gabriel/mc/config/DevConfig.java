59908086fe306a473bc4da8641317b4733e91e24
Gabriel Longhi <gabrielkuodrek@hotmail.com>
2019-07-26 08:47:17 -0300

    Dando erro ao enviar email, emailService foi comentado no PedidoService

--- src/main/java/com/gabriel/mc/config/DevConfig.java
+++ src/main/java/com/gabriel/mc/config/DevConfig.java
@@ -1,6 +1,8 @@
 package com.gabriel.mc.config;
 
 import com.gabriel.mc.services.DBService;
+import com.gabriel.mc.services.EmailService;
+import com.gabriel.mc.services.SmtpEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -28,5 +30,10 @@ public class DevConfig {
         dbService.instantiateTestDatabase();
 
         return true;
+    }
+
+    @Bean
+    public EmailService emailService() {
+        return new SmtpEmailService();
     }
 }
