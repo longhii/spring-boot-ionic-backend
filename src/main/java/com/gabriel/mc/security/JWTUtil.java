fef220fe04688c4dc190e4f3da8610d58f55d8b0
Gabriel Longhi <gabriellonghi07@jallcards.com.br>
2019-07-28 18:44:54 -0300

    Implementando autenticacao e geracao de token

---
+++ src/main/java/com/gabriel/mc/security/JWTUtil.java
@@ -0,0 +1,28 @@
+package com.gabriel.mc.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+
+
+    @Value(value = "${jwt.secret}")
+    private String secret;
+
+    @Value(value = "${jwt.expiration}")
+    private Long expiration;
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(SignatureAlgorithm.HS512, secret.getBytes())
+                .compact();
+    }
+
+}
